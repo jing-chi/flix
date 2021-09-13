825dfa65917a3797b43fda86fdb979e8f6c93767
jing-chi <jchi3@buffalo.edu>
2021-09-12 23:36:47 -0400

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Jing Jing Chi on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
